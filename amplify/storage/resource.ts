5366e824311859c9740d05aa50e2cd51192714ba
gkupsaw <griffin_kupsaw@brown.edu>
2025-09-03 22:40:52 -0400

    Version S3 objs

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,6 +2,7 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
     name: 'griffins-recipes',
+    versioned: true,
     access: (allow) => ({
         'recipe-data/*': [allow.authenticated.to(['read', 'write', 'delete']), allow.guest.to(['read'])],
         'private-recipe-data/*': [allow.authenticated.to(['read', 'write', 'delete'])],
